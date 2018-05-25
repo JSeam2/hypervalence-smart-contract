59f309cace4f91e5fcab36256fb9162662af0dcb
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2018-05-25 15:29:53 +0800

    add token contract

--- contracts/TokenAuction.sol
+++ contracts/TokenAuction.sol
@@ -1,6 +1,5 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
-import "./ERC721Basic.sol";
 import "./ArtistToken.sol";
 
 contract TokenAuction is ArtistToken {
@@ -8,51 +7,42 @@ contract TokenAuction is ArtistToken {
   struct Auction {
     address seller;
     address artist;
-    uint256 startPrice;
-    uint256 endPrice;
+    uint256 price;
     uint64 startedAt;
     uint8 royaltyPercentage;
   }
 
-	struct ArtistStats {
-		address artist;
-		uint256 endPrice;
-		uint64 endTime;
-	}
-
   // Open Auctions
-  mapping (uint256 => Auction) public tokenIdToOpenAuction;
+  mapping (uint256 => Auction) public tokenIdToAuction;
 
-	ArtistStats[] public artistStats;
-  
-
-  event AuctionCreated(uint256 tokenId, address artist, uint256 startingPrice, uint256 endingPrice);
+  event AuctionCreated(uint256 tokenId, address artist, uint256 price);
   event AuctionSuccessful(uint256 tokenId, address artist, uint256 totalPrice, address winner);
   event AuctionCancelled(uint256 tokenId);
 
   // escrow the NFT assigns ownership to contract
   function _escrow(address _owner, uint256 _tokenId) internal {
-  ERC721BasicToken.transferFrom(_owner, this, _tokenId);
+    approve(this, _tokenId);
+    transferFrom(_owner, this, _tokenId);
   }
 
   // transfer NFT owned by contract to another address
   function _transfer(address _receiver, uint256 _tokenId) internal {
-    ERC721BasicToken.transferFrom(this, _receiver, _tokenId);
+    approve(_receiver, _tokenId);  
+    transferFrom(this, _receiver, _tokenId);
   }
 
   // add auction
   function _addAuction(uint256 _tokenId, Auction _auction) internal {
-    tokenIdToOpenAuction[_tokenId] = _auction;
+    tokenIdToAuction[_tokenId] = _auction;
     emit AuctionCreated(
       uint256(_tokenId),
       address(_auction.artist),
-      uint256(_auction.startPrice),
-      uint256(_auction.endPrice)
+      uint256(_auction.price)
     );
   }
 
   function _removeAuction(uint256 _tokenId) internal {
-    delete tokenIdToOpenAuction[_tokenId];
+    delete tokenIdToAuction[_tokenId];
   }
 
   function _cancelAuction(uint256 _tokenId, address _seller) internal {
@@ -65,17 +55,14 @@ contract TokenAuction is ArtistToken {
     return (_auction.startedAt > 0);
   }
 
-  function _bid(uint256 _tokenId, uint256 _bidAmount)
-    internal returns (uint256) {
-
-    Auction storage auction = tokenIdToOpenAuction[_tokenId];
+  function _bid(uint256 _tokenId, uint256 _bidAmount) internal {
+    Auction storage auction = tokenIdToAuction[_tokenId];
     require(_isOnAuction(auction));
-    uint256 price = auction.startPrice;
+    uint256 price = auction.price;
     require(_bidAmount >= price);
     // save values
     address seller = auction.seller;
     address artist = auction.artist; 
-    uint256 endPrice = _bidAmount;
     uint256 royaltyPercentage = uint256(auction.royaltyPercentage);
     _removeAuction(_tokenId);
 
@@ -84,40 +71,35 @@ contract TokenAuction is ArtistToken {
       uint256 sellerProceeds = price - artistCut;
       seller.transfer(sellerProceeds);
       artist.transfer(artistCut);
-
-	  artistStats.push(ArtistStats(artist, endPrice, uint64(now)));
     }
     
-    emit AuctionSuccessful(_tokenId, artist, endPrice, msg.sender);
-
-    return endPrice;
+    emit AuctionSuccessful(_tokenId, artist, _bidAmount, msg.sender);
     
   }
 
 
   function createAuction(
     uint256 _tokenId,
-    uint256 _startingPrice,
-    uint256 _endingPrice
+    uint256 _price
   ) public onlyOwnerOf(_tokenId) {
 
-		require(_startingPrice == uint256(uint128(_startingPrice)));
-		require(_endingPrice == uint256(uint128(_endingPrice)));
+    require(_price == uint256(uint128(_price)));
 
-		_escrow(msg.sender, _tokenId);
-		address _artist = ArtistToken.tokenToArtistAddress[_tokenId];
-		uint8 _royaltyPercentage = ArtistToken.tokenToRoyaltyPercentage[_tokenId];
+    _escrow(msg.sender, _tokenId);
+    // this is not accessed
+    address _artist = getArtistAddress(_tokenId);
+    // this mapping is not accessed try round brackets
+    uint8 _royaltyPercentage = getRoyaltyPercentage(_tokenId);
 
-		Auction memory auction = Auction(
-			msg.sender,
-			_artist,
-			_startingPrice,
-			_endingPrice,
-			uint64(now),
-			uint8(_royaltyPercentage)
-		);
+    Auction memory auction = Auction(
+      msg.sender,
+      _artist,
+      _price,
+      uint64(now),
+      _royaltyPercentage
+    );
     
-		_addAuction(_tokenId, auction);
+    _addAuction(_tokenId, auction);
   }
 
   function bid(uint256 _tokenId) external payable {
@@ -126,7 +108,7 @@ contract TokenAuction is ArtistToken {
   }
 
   function cancelAuction(uint256 _tokenId) public {
-    Auction storage auction = tokenIdToOpenAuction[_tokenId];
+    Auction storage auction = tokenIdToAuction[_tokenId];
     require(_isOnAuction(auction));
     address seller = auction.seller;
     require(msg.sender == seller);
@@ -138,40 +120,20 @@ contract TokenAuction is ArtistToken {
     view
     returns
   (
-    address seller,
-    address artist,
-    uint256 startingPrice,
-    uint256 endingPrice,
-    uint256 startedAt,
-    uint256 royaltyPercentage
+    address,
+    address,
+    uint256,
+    uint64,
+    uint8
   ) {
-    Auction storage auction = tokenIdToOpenAuction[_tokenId];
+    Auction storage auction = tokenIdToAuction[_tokenId];
     require(_isOnAuction(auction));
     return (
-      auction.seller,
-	  auction.artist,
-      auction.startPrice,
-      auction.endPrice,
-      auction.startedAt,
-	  auction.royaltyPercentage
+        auction.seller,
+      auction.artist,
+        auction.price,
+        auction.startedAt,
+      auction.royaltyPercentage
     );
-      
   }
-    
-  function getArtistStats(uint256 index)
-    external
-    view
-    returns
-  (
-    address artist,
-    uint256 endingPrice,
-    uint64 endTime
-  ) {
-    ArtistStats storage stats = artistStats[index];
-    return (
-	  stats.artist,
-      stats.endPrice,
-      stats.endTime
-    );
-    }
 }
