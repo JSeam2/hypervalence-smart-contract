013615b043ffaf09bd54926b96986aa759dc9a86
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-05-28 01:56:50 +0800

    createAuction and cancelAuction and bid is working, auctionClose and transfer functionality to be fixed

--- contracts/TokenAuction.sol
+++ contracts/TokenAuction.sol
@@ -21,26 +21,46 @@ contract TokenAuction is ArtistToken {
   // Completed auctions
   Auction[] public completedAuctions;
 
-  event AuctionCreated(uint256 tokenId, address artist, uint256 startPrice, uint64 duration);
+  event AuctionCreated(uint256 tokenId, address artist, uint256 startPrice, uint64 auctionStart, uint64 auctionClose);
   event AuctionBidIncreased(uint256 tokenId, address artist, address bidder, uint256 startPrice, uint256 endPrice);
   event AuctionSuccessful(uint256 tokenId, address artist, uint256 endPrice, address winner);
   event AuctionCancelled(uint256 tokenId);
 
   // escrow the NFT assigns ownership to contract
   function _escrow(address _owner, uint256 _tokenId) internal {
-    //approve(this, _tokenId);
-    transferFrom(_owner, this, _tokenId);
+    // clear approvals
+    clearApproval(_owner, _tokenId);
+    
+    // remove token from   
+    super.removeTokenFrom(_owner, _tokenId);
+    
+    // transfer ownership
+    super.addTokenTo(this, _tokenId);
+    tokenOwner[_tokenId] = this;
+    ownedTokensCount[this] = ownedTokensCount[this].add(1);
+    
+    emit Transfer(_owner, this, _tokenId);
   }
 
   // transfer NFT owned by contract to another address
   function _transfer(address _receiver, uint256 _tokenId) internal {
-    //approve(_receiver, _tokenId);  
-    transferFrom(this, _receiver, _tokenId);
+    // clear approvals
+    clearApproval(this, _tokenId);
+    
+    // remove token from   
+    super.removeTokenFrom(this, _tokenId);
+    
+    // transfer ownership
+    addTokenTo(_receiver, _tokenId);
+    tokenOwner[_tokenId] = _receiver;
+    ownedTokensCount[_receiver] = ownedTokensCount[_receiver].add(1);
+    
+    emit Transfer(this, _receiver, _tokenId);
   }
 
   // check if the auction is on
   function _isOnAuction(Auction storage _auction) internal view returns (bool) {
-    return (_auction.startedAt > 0);
+    return (_auction.auctionStart > 0);
   }
 
   // creates an Auction
@@ -55,6 +75,9 @@ contract TokenAuction is ArtistToken {
     // get values from inherited contract ArtistToken
     address _artist = getArtistAddress(_tokenId);
     uint8 _royaltyPercentage = getRoyaltyPercentage(_tokenId);
+    
+    uint64 _auctionStart = now;
+    uint64 _auctionEnd = now + duration;
 
     Auction memory _auction = Auction(
       msg.sender,
@@ -62,8 +85,8 @@ contract TokenAuction is ArtistToken {
       msg.sender,
       _startPrice,
       _startPrice,
-      uint64(now),
-      uint64(now) + _duration,
+      _auctionStart,
+      _auctionEnd,
       _royaltyPercentage
     );
 
@@ -75,7 +98,8 @@ contract TokenAuction is ArtistToken {
       uint256(_tokenId),
       address(_auction.artist),
       uint256(_auction.startPrice),
-      uint256 (_auction.duration)
+      uint64(_auction.auctionStart),
+      uint64(_auction.auctionClose)
     );
   }
 
@@ -83,7 +107,8 @@ contract TokenAuction is ArtistToken {
     Auction storage auction = tokenIdToAuction[_tokenId];
     require(msg.sender == auction.seller);
     require(_isOnAuction(auction));
-    _transfer(_seller, _tokenId);
+    require(auction.auctionClose >= now);
+    _transfer(auction.seller, _tokenId);
     delete tokenIdToAuction[_tokenId];
     emit AuctionCancelled(_tokenId);
   }
@@ -115,6 +140,10 @@ contract TokenAuction is ArtistToken {
     Auction storage auction = tokenIdToAuction[_tokenId];
     require(now > auction.auctionClose);
     require(_isOnAuction(auction));
+    // add to completedAuctions
+    completedAuctions.push(auction);
+    
+    // delete auction
     delete tokenIdToAuction[_tokenId];
 
     uint256 artistCut =  auction.endPrice * auction.royaltyPercentage/100;
@@ -125,12 +154,12 @@ contract TokenAuction is ArtistToken {
     auction.artist.transfer(artistCut);
     _transfer(auction.topBidder, _tokenId);
 
-    // annouce event
-    emit AuctionSuccessful(_tokenId, address auction.artist, uint256 auction.endPrice, address auction.topBidder);
+    // announce event
+    emit AuctionSuccessful(_tokenId, auction.artist, auction.endPrice, auction.topBidder);
   }
 
   function getOpenAuction(uint256 _tokenId)
-    external
+    public
     view
     returns
   (
@@ -151,7 +180,34 @@ contract TokenAuction is ArtistToken {
         auction.topBidder,
         auction.startPrice,
         auction.endPrice,
-        auction.auctionStart
+        auction.auctionStart,
+        auction.auctionClose,
+        auction.royaltyPercentage
+    );
+  }
+  
+    function getCompletedAuction(uint256 index)
+    public
+    view
+    returns
+  (
+    address,
+    address,
+    address,
+    uint256,
+    uint256,
+    uint64,
+    uint64,
+    uint8
+  ) {
+    Auction storage auction = completedAuctions[index];
+    return (
+        auction.seller,
+        auction.artist,
+        auction.topBidder,
+        auction.startPrice,
+        auction.endPrice,
+        auction.auctionStart,
         auction.auctionClose,
         auction.royaltyPercentage
     );
