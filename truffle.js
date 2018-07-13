af9a2b79eb08ce1b8acb9f5e07f970c810013c83
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-07-14 00:11:39 +0800

    edits in truffle.js

--- truffle.js
+++ truffle.js
@@ -2,18 +2,18 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    ganache: {
-	  host: "127.0.0.1",
-	  port: 7545,
-	  network_id: "*" // match any network id
-	},
-	
-	ropsten: {
-	  host: "127.0.0.1",
-	  port: 8545,
-	  network_id: 3,
-	  gas: 4700000
-	},
+    local: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // match any network id
+    },
+    
+    testnet: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 3,
+      gas: 4700000
+    }
   }
 };
 
