e6c5ae0c62163fcc25d22148104447b8faa2364d
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-07-17 23:12:13 +0800

    more test cases

--- test/TokenAuctionTest.js
+++ test/TokenAuctionTest.js
@@ -1,42 +1,16 @@
 /**
 * TokenAuctionTest
+* Unit tests for auction functionality
 */
 
 const TokenAuction = artifacts.require("TokenAuction");
 
 contract("TokenAuction", (accounts) => {
-	it("should have 0 tokens initially", () => {
-		return TokenAuction.deployed().then(instance => {
-			return instance.totalSupply.call();
-		}).then(supply => {
-			assert.equal(supply.valueOf(), 0, "Started with more than 0???"); 
-		});	
-	});
-
-	it("should have 1 token in totalSupply after minting", () => {
-		return TokenAuction.deployed().then(instance => {
-			instance.mint("Test Token", "Test Description");
-			return instance.totalSupply.call();
-		}).then(supply => {
-			assert.equal(supply.valueOf(), 1, "TotalSupply should not be more than 1");
-		});
-	});
-
-	it("should transfer without errors", () => {
-		return TokenAuction.deployed().then(instance => {
-			instance.mint("Test Token", "Test Description");
-			instance.transferFrom(accounts[0], accounts[1], 0);
-			return instance.ownerOf(0);
-		}).then(addr => {
-			assert.equal(accounts[1], addr, "Addresses should equal");
-		});
-	});
-
 	it("should start an auction and contract to act as escrow", () => {
 		return TokenAuction.deployed().then(instance => {
 			instance.mint("Test Token", "Test Description");
-			instance.createAuction(1, 1000, 100);
-			return instance.ownerOf(1);
+			instance.createAuction(0, 1000, 100);
+			return instance.ownerOf(0);
 		}).then(addr => {
 			assert.equal(addr, TokenAuction.address, "Owner of token after createAuction is not contract");
 		});
@@ -45,9 +19,9 @@ contract("TokenAuction", (accounts) => {
 	it("should be able to cancel an auction", () => {
 		return TokenAuction.deployed().then(instance => {
 			instance.mint("Test Token", "Test Description");
-			instance.createAuction(2, 1000, 100);
-			instance.cancelAuction(2);
-			return instance.ownerOf(2);
+			instance.createAuction(1, 1000, 100);
+			instance.cancelAuction(1);
+			return instance.ownerOf(1);
 		}).then(addr => {
 			assert.equal(addr, accounts[0], "Owner of token after cancelAuction is not the original owner");
 		});
